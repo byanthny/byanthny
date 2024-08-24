236cecd5b1b5207743fd67698aba3b35ec81a482
Anthony Das <dev@anthnydas.com>
2024-08-24 11:53:35 -0400

    remade hugo site

--- byanthny/app/page.tsx
+++ byanthny/app/page.tsx
@@ -1,9 +1,13 @@
+import Items from "@/components/Items";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>byanthny</h1>
-    </main>
+    <div className="px-[25%] pt-[5%]">
+      <main className="">
+        <h1 className="text-2xl font-bold py-4">@byanthny</h1>
+        <Items />
+      </main>
+    </div>
   );
 }
---
+++ byanthny/components/Items.tsx
@@ -0,0 +1,216 @@
+const Items = () => {
+  return (
+    <ul className="list-disc">
+      <li>info</li>
+      <ul>
+        <li>anthony das</li>
+        <ul>
+          <li>
+            <a href="/resume/Resume.pdf">resume</a> &amp;{" "}
+            <a href="mailto:me@anthnydas.com">email</a>
+          </li>
+          <li>
+            b.s. in computer science from <a href="https://jhu.edu">jhu</a>
+          </li>
+        </ul>
+        <li>socials</li>
+        <ul>
+          <li>
+            <a href="https://github.com/byanthny/">github</a>
+          </li>
+          <li>
+            <a href="https://gitlab.com/byanthny/">gitlab</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/byanthny/">linkedin</a>
+          </li>
+        </ul>
+      </ul>
+      <li>blog</li>
+      <li>projects</li>
+      <ul>
+        <li>current</li>
+        <ul>
+          <li>
+            <a href="https://github.com/byanthny/yugo">yugo</a>: basic hugo
+            theme
+          </li>
+          <ul>
+            <li>your go to hugo theme</li>
+          </ul>
+          <li>
+            <a href="https://github.com/pinkilo">pinkilo</a>: coming soon
+          </li>
+          <ul>
+            <li>see link above for projects</li>
+          </ul>
+          <li>
+            <a href="https://github.com/byanthny/anthnydas.com">anthnydas</a>:
+            tbd
+          </li>
+        </ul>
+        <li>backlog/maintaining</li>
+        <ul>
+          <li>
+            <a href="https://github.com/byanthny/.dotfiles">.dotfiles</a>: my
+            dotfiles
+          </li>
+          <li>
+            <a href="https://github.com/byanthny/opengl">opengl</a>: learning
+            opengl by making a game engine
+          </li>
+          <ul>
+            <li>c++</li>
+          </ul>
+          <li>
+            <a href="https://github.com/byanthny/macropad-rp2040">
+              rp2040 macropad
+            </a>
+            : custom macropad with rp2040
+          </li>
+          <ul>
+            <li>
+              code for custom macropad built with adafruit's macropad rp2040 kit
+            </li>
+            <li>python</li>
+          </ul>
+        </ul>
+        <li>archive</li>
+        <ul>
+          <li>
+            2022 - <a href="https://gitlab.com/JonoAugustine/on-it">on-it</a>:
+            minimal note taking and to-do web application
+          </li>
+          <ul>
+            <li>but overly 'complicated' ui</li>
+            <li>node, react, sass, typescript, mongoDB</li>
+            <li>
+              <a href="https://gitlab.com/JonoAugustine/on-it/-/graphs/master">
+                members
+              </a>
+            </li>
+          </ul>
+          <li>
+            2022 - <a href="https://github.com/byanthny/lyric-bot">lyric-bot</a>
+            [unfinished]: a discord bot
+          </li>
+          <ul>
+            <li>discord bot for music lyrics</li>
+            <li>python</li>
+          </ul>
+          <li>
+            2022 - <a href="https://jhu-bonsai.herokuapp.com/">bonsai</a> [no
+            public repo]: resource and collaboration hub for JHU students
+          </li>
+          <ul>
+            <li>class project from uni, repo owned by uni</li>
+            <li>node, react, tailwindcss, express, mongoDB, typescript</li>
+            <li>members: class group</li>
+          </ul>
+          <li>
+            2021 - <a href="https://github.com/sf11047/anime-db">anime-db</a>:
+            an database of anime data and site for analysis
+          </li>
+          <ul>
+            <li>class project from uni</li>
+            <li>php, mySQL</li>
+            <li>
+              <a href="https://github.com/sf11047/anime-db/graphs/contributors">
+                members
+              </a>
+            </li>
+          </ul>
+          <li>
+            2020 - <a href="">drosophila</a> [no public repo]: helped expand
+            computational models of Drosophila neuron behavior
+          </li>
+          <ul>
+            <li>research assistant from uni, contributor</li>
+            <li>python, NUERON</li>
+            <li>members: lab</li>
+          </ul>
+          <li>
+            2020 - <a href="">pintOS</a> [no public repo]: operating system
+          </li>
+          <ul>
+            <li>class project from uni based on pintOS, repo owned by uni</li>
+            <li>c, c++</li>
+            <li>members: class group</li>
+          </ul>
+          <li>
+            2020 - <a href="">CoGS</a> [no public repo]: android application for
+            study groups
+          </li>
+          <ul>
+            <li>class project from uni, repo owned by uni</li>
+            <li>android development, java</li>
+            <li>members: class group</li>
+          </ul>
+          <li>
+            2019 - <a href="https://github.com/byanthny/Ouch">ouch</a>: a
+            simulation where you and your friends can exist in an existence
+            together
+          </li>
+          <ul>
+            <li>over complicated messaging rooms</li>
+            <li>javascript, less, html</li>
+            <li>
+              <a href="https://github.com/JonoAugustine">members</a>
+            </li>
+          </ul>
+          <li>
+            2019 - <a href="https://github.com/byanthny/Athena">athena</a>{" "}
+            [unfinished]: a modern css framework
+          </li>
+          <ul>
+            <li>
+              abandonded because better projects exist and learned what i needed
+            </li>
+            <li>less, html</li>
+          </ul>
+          <li>
+            2018 -{" "}
+            <a href="https://www.youtube.com/watch?v=DPesVSQQUqY">
+              leadingedge labs &amp; r3d
+            </a>
+            : game based therapy for custom 3-D printed myoelectric prosthethics
+            + project website
+          </li>
+          <ul>
+            <li>built during after school program from highschool</li>
+            <li>unity, leapmotion, arduino, c++, html/css/js</li>
+          </ul>
+        </ul>
+      </ul>
+      <li>site info</li>
+      <ul>
+        <li>
+          <a href="index.xml">rss</a>
+        </li>
+        <li>
+          <a href="https://github.com/byanthny/byanthny">github repo</a>
+        </li>
+        <li>
+          built with <a href="https://gohugo.io/">Hugo</a>.
+        </li>
+        <li>
+          theme:{" "}
+          <a href="https://github.com/Masellum/hugo-theme-nostyleplease">
+            masellum/no-style-please
+          </a>
+          .
+        </li>
+        <li>custom theme coming in future...</li>
+        <li>site updates</li>
+        <ul>
+          <li>
+            <span>2022-10-18</span>
+            <a href="https://byanthny.com/site/init/">init website</a>
+          </li>
+        </ul>
+      </ul>
+    </ul>
+  );
+};
+
+export default Items;
