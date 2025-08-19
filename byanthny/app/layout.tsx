074e544707553645bd5e7a97d54be4dea7b5dc1e
Anthony Das <dev@anthnydas.com>
2025-08-19 19:23:13 -0400

    update site to accommodate new NavBar

--- byanthny/app/layout.tsx
+++ byanthny/app/layout.tsx
@@ -1,23 +1,25 @@
-import { Analytics } from "@vercel/analytics/react";
-import type { Metadata } from "next";
-import "./globals.css";
+import NavBar from "@/components/NavBar"
+import { Analytics } from "@vercel/analytics/react"
+import type { Metadata } from "next"
+import "./globals.css"
 
 export const metadata: Metadata = {
   title: "byanthny | my work",
   description: "my work",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body className="bg-black text-white">
+        <NavBar />
         {children}
         <Analytics />
       </body>
     </html>
-  );
+  )
 }
