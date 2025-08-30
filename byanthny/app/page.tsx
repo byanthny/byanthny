bdcd473e39199c59aa2c3a97e3fc326124555697
Anthony Das <dev@anthnydas.com>
2025-08-30 14:33:58 -0400

    fix: top padding

--- byanthny/app/page.tsx
+++ byanthny/app/page.tsx
@@ -2,7 +2,7 @@ import Items from "@/components/Items"
 
 export default function Home() {
   return (
-    <div className="px-[25%] pt-[5%]">
+    <div className="px-[25%] pt-20">
       <main className="">
         {/* <h1 className="text-2xl font-bold py-4">@byanthny</h1> */}
         <Items />
