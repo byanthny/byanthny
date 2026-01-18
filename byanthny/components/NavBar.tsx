7910374e4ac429f251a721e3171743ef8811554f
Anthony Das <dev@anthnydas.com>
2026-01-18 17:10:00 -0500

    feat: updated NavBar

--- byanthny/components/NavBar.tsx
+++ byanthny/components/NavBar.tsx
@@ -1,10 +1,21 @@
+'use client'
+
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 const NavBar = () => {
+  const pathname = usePathname()
+  const isLogPage = pathname === '/log'
+
   return (
     <nav className={`fixed top-0 w-full p-6 px-[8%] md:px-[20%] pt-3 z-50`}>
       <div className="bg-neutral-900/50 backdrop-blur-md rounded-lg w-full flex flex-row justify-between items-center border-neutral-700/10 border-2 px-6 py-3">
-        <h1 className="text-lg font-bold">@byanthny</h1>
+        <h1 className="text-lg">
+          <Link href="/" className="text-white font-bold">
+            @byanthny
+          </Link>
+          {isLogPage && ' - log'}
+        </h1>
         <div className="flex flex-row items-center gap-3">
           <Link href="/" className="text-neutral-300 hover:text-white">
             index
